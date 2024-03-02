95f6f763f8f4a711099bea8c172541a3560e1c5e
lubluosen <145040416+lubluosen@users.noreply.github.com>
2024-03-02 22:43:25 +0600

    Update main.py
    Добавлена функция prime
    Изменены названия переменных

--- homework_01/main.py
+++ homework_01/main.py
@@ -12,23 +12,30 @@ print(power_numbers(1, 2, 5, 7))
 
 
 # filter types
-from sympy import isprime
+def prime(num):
+    if num > 1:
+        for i in range(2, int(num / 2) + 1):
+            if not num % i:
+                return None
+        return num
+    return None
+
+
 ODD = "odd"
 EVEN = "even"
 PRIME = "prime"
 
 
-def filter_numbers(num_list, p):
+def filter_numbers(num_list, num):
     result = []
-    for num in num_list:
-        if p == "odd" and num % 2 != 0:
-            result.append(num)
-        elif p == "even" and num % 2 == 0:
-            result.append(num)
-        elif p == "prime" and isprime(num):
-            result.append(num)
+    for numbers in num_list:
+        if num == "odd" and numbers % 2 != 0:
+            result.append(numbers)
+        elif num == "even" and numbers % 2 == 0:
+            result.append(numbers)
+        elif num == "prime" and prime(numbers):
+            result.append(numbers)
     return result
 
 
 print(filter_numbers([5, 8, 3, 6, 9], PRIME))
-
