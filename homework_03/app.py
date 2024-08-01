628c66049011c35b1c1d1b20d114409668c9dd5a
lubluosen <145040416+lubluosen@users.noreply.github.com>
2024-08-01 10:49:36 +0600

    Add files via upload

--- homework_03/app.py
+++ homework_03/app.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
+@app.get("/")
+def index_page():
+    return {"Greatings": "Hello, World!"}
+
+
 @app.get("/ping/")
-def view():
+def ping_page():
     return {"message": "pong"}
