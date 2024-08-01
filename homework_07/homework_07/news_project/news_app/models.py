aa435e51701d407ebd08eaaf4b3f6fa21055d652
lubluosen <145040416+lubluosen@users.noreply.github.com>
2024-08-01 11:11:29 +0600

    Add files via upload

---
+++ homework_07/homework_07/news_project/news_app/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
